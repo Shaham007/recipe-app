a4cb82ed5a4c92c5d7a1349b305837c5862ac74e
Shaha007 <Shaham.prince56@gmail.com>
2022-01-30 22:20:10 +0100

    final  commit

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function getMealList(){
                         </div>
                         <div class="meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href="#" class="recipe-btn">Get Recipe</a>
+                            <a href="#" class="recipe-btn">Show info</a>
                         </div>
                     </div>
                 `;
