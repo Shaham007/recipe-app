303ee5cd6d9479d0e8402d799559a0e19a01fa70
Shaha007 <Shaham.prince56@gmail.com>
2022-01-30 20:37:46 +0100

    final  commit

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # recipe-app
 
-A Food Recipe app with a recipe generator by searching any recipe name.
+!! A Food Recipe app with a recipe generator by searching any recipe name!!
 
-getting all the data from certein Api.
+This project have some good functionalities, when you search for any meal it provides all of data from an Api.
+
+The data is in json format and is fully destructured by JavaScript and provided in different parts.
+
+When you get all data from Api click on the get Recipe button it will open a Modal where it will shoes recipe title, its category, instruction, its image and also a youtube video of that recipe, this all data is from an Api.
+
+when you click on that watch video button it has a functionality that the video will be opened on other tab of your browser.
+
+And can close the modal by a close button on top right.
 
 Project is fully responsive and can be used in mobile view.
 
--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const mealDetailsContent = document.querySelector(".meal-details-content");
 
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
+const mealDetails = document.querySelector(".meal-details")
+
+const detailsMain = document.querySelector(".details-main")
+
 // EVENT LISTENERS 
 
-searchBtn.addEventListener('click' , getMealList);
+searchBtn.addEventListener('click', getMealList);
+
+mealList.addEventListener('click', getMealRecipe);
+
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetails.classList.remove("showRecipe"),
+    detailsMain.classList.remove("showRecipe")
+});
+
+
+
+// getting API
 
 function getMealList(){
     let searchInputTxt = 
@@ -20,7 +35,7 @@ function getMealList(){
         if(data.meals){
             data.meals.forEach(meal => {
                 html += `
-                    <div class="meal-item" data-id = "${meal.idmeal}">
+                    <div class="meal-item" data-id = "${meal.idMeal}">
                         <div class="meal-img">
                             <img src="${meal.strMealThumb}" alt="" width="300px" height="200px">
                         </div>
@@ -33,9 +48,46 @@ function getMealList(){
             });
         } else{
             html = "Sorry we did'nt find any meal of this ingredient, please type other( chicken , egg , beef ) ";
+            mealList.classList.add("notFound");
         }
 
         mealList.innerHTML = html;
-        mealList.classList.add("notFound");
+        
     })
+}
+
+// get recipe of the meal
+
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains("recipe-btn")){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+// create a modal
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal= meal[0];
+    let html = `
+               <h2 class="recipe-title">${meal.strMeal}</h2>
+               <p class="recipe-category">${meal.strCategory}</p>
+               <div class="recipe-instruction">
+                   <h3>Instruction:</h3>
+                    <p>${meal.strInstructions}</p>
+                </div>
+                <div class="recipe-meal-img">
+                    <img src="${meal.strMealThumb}" alt="" width="300px" height="200px">
+                </div>
+                <div class="recipe-link">
+                    <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+                </div>
+    `;
+
+    mealDetailsContent.innerHTML = html;
+    mealDetails.classList.add("showRecipe");
+    detailsMain.classList.add("showRecipe");
 }
